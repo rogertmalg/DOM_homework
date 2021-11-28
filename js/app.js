67baaf7bbc8808470f41e6ddb2b0e1f98ad237c0
rogertmalg <roger.malgueira@outlook.com>
2021-11-28 20:44:32 +0000

    save button working

--- js/app.js
+++ js/app.js
@@ -1,3 +1,20 @@
 document.addEventListener('DOMContentLoaded', () => {
     console.log('JavaScript loaded');
+
+    document.querySelector('#watchlist-form').addEventListener('submit', handleFormSubmit);
   })
+
+  const handleFormSubmit = function (event) {
+    event.preventDefault();
+  
+    const newTitle = `${event.target.title.value}`;
+    const newType = `${event.target.type.value}`;
+    const newPlatform = `${event.target.platform.value}`;
+    const newListItem = document.createElement('li');
+    newListItem.classList.add("single-item")
+    const list = document.querySelector('#watchlist');
+    // newListItem.textContent = `Title: ${newTitle} Author: ${newAuthor} Category: ${newCategory}`;
+    newListItem.innerHTML = `<h2>${newTitle}</h2><br><h3>${newType}</h3><br><h4>${newPlatform}</h4>`;
+    list.appendChild(newListItem);
+    this.reset()
+  };
